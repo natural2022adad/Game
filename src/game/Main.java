e7ce72840d66eb5a18ebeecb7360fea83c752dfa
git2022ada <h.cpa.park@gmail.com>
2022-05-25 13:29:53 +0900

    マタンゴA,B登場

--- src/game/Main.java
+++ src/game/Main.java
@@ -16,13 +16,20 @@ public class Main
 		
 		m.name = "あだちゃん";
 		m.hp = 50;
+		
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.Suffix = 'A';
+		
+		Matango m2 = new Matango();
+		m2.hp = 48;
+		m2.Suffix = 'B';
+		
 		System.out.println("酒好き" + m.name + "を生み出しました！");
 		h.sit(50);
 		System.out.println(h.hp);
-		h.slip();
-		System.out.println(h.hp);
-		h.sit(5);
-		System.out.println(h.hp);
+		m1.run();
+		m2.run();
 		h.run();
 		
 		m.slip();
--- src/game/Matango.java
+++ src/game/Matango.java
@@ -4,5 +4,9 @@ public class Matango {
 	
 	int hp;
 	final int LEVEL = 10; 
+	char Suffix;
+	public void run() {
+		System.out.println("お化けキノコ" + this.Suffix + "はにげだした");
+	}
 	
 }
