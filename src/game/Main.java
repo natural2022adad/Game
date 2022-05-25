c8aa8a57b8061749741e769adad999260cd99e12
git2022ada <h.cpa.park@gmail.com>
2022-05-25 11:47:42 +0900

    プログラマーと酒好きを動かした！

--- src/game/Main.java
+++ src/game/Main.java
@@ -11,6 +11,26 @@ public class Main
 		h.name = "あだち";
 		h.hp = 1000;
 		System.out.println("プログラマー" + h.name + "を生み出しました！");
+		
+		Hero m = new Hero();
+		
+		m.name = "あだちゃん";
+		m.hp = 50;
+		System.out.println("酒好き" + m.name + "を生み出しました！");
+		h.sit(50);
+		System.out.println(h.hp);
+		h.slip();
+		System.out.println(h.hp);
+		h.sit(5);
+		System.out.println(h.hp);
+		h.run();
+		
+		m.slip();
+		System.out.println(m.hp);
+		m.sleep();
+		System.out.println(h.hp);
+		System.out.println(m.hp);
+		
 	}
 
 }
