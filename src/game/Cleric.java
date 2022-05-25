d9a4a63647090f159295960e010304e77941bc81
git2022ada <h.cpa.park@gmail.com>
2022-05-25 14:31:39 +0900

    聖職者「クレリック」登場！

---
+++ src/game/Cleric.java
@@ -0,0 +1,35 @@
+package game;
+
+public class Cleric 
+{
+	
+	String name;
+	int hp = 50;
+	int mp = 10;
+	
+	int max_hp = 50;
+	int max_mp = 10;
+	public void selfAid() 
+	{
+		this.mp -= 5;
+		this.hp = this.max_hp;
+	}
+	
+	public void pray(int sec) 
+	{
+		int r = new java.util.Random().nextInt(3);
+		System.out.println(r);
+		int p = (sec + r);
+		System.out.println(p);
+		if (this.mp <= 10) 
+		{
+			this.mp += p; 
+			if (this.mp >= 10) 
+			{
+				this.mp =10;
+			}
+		}
+	}	
+
+}
+ 
