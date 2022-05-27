7fe21464dccc286590b2339740b753a8ffa01e89
git2022ada <h.cpa.park@gmail.com>
2022-05-27 13:29:25 +0900

    メインに引数を渡す

--- src/game9/Main.java
+++ src/game9/Main.java
@@ -6,15 +6,13 @@ public class Main {
 		Sword s = new Sword();
 		s.name = "炎の剣";
 		s.damage = 10;
-		Hero h1 = new Hero();
-		h1.name = "ミナト";
-		
+		Hero h1 = new Hero("ミナト");
+
 		h1.sword = s;
 		System.out.println("現在の武器は" + h1.sword.name);
 		h1.attack();
 		
-		Hero h2 = new Hero();
-		h2.name = "アサカ";
+		Hero h2 = new Hero("アサカ");
 		
 		Wizard w = new Wizard();
 		w.name = "スガワラ";
@@ -23,8 +21,7 @@ public class Main {
 		w.heal(h2);
 		w.heal(h2);
 		
-		Hero h = new Hero();
-		h.name = "アダチん";
+		Hero h = new Hero("アダチん");
 		System.out.println(h.name + "HP" + h.hp);
 		System.out.println(h1.name + "HP" + h1.hp);
 		System.out.println(h2.name + "HP" + h2.hp);
